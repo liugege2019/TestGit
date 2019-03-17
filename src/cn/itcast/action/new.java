1b1b146660605c126f8f85c0e9852854485e46cd
liugege2019 <1540122432@qq.com>
2019-03-17 19:15:32 +0800

    新建的类

---
+++ src/cn/itcast/action/new.java
@@ -0,0 +1,9 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class OrderAction extends ActionSupport{
+	   public String execute(){
+	    	return "success";
+	    }
+}
