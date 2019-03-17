2a3577ac01552c3696bd7b0c8d6e3f0ac752f976
liugege2019 <1540122432@qq.com>
2019-03-17 19:17:35 +0800

    more update

--- src/cn/itcast/action/new.java
+++ src/cn/itcast/action/new.java
@@ -2,8 +2,9 @@ package cn.itcast.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class OrderAction extends ActionSupport{
+public class new extends ActionSupport{
 	   public String execute(){
 	    	return "success";
+	    	System.out.println('test.....');
 	    }
 }
